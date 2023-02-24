93e0a51f741fecb6d0b2103494112d3dfbcab232
Shaurya <shauryachawan4646@gmail.com>
2023-02-24 20:32:37 +0530

    Added About Us Page and Changed background color of navbar to black

---
+++ frontend/src/components/MTTCard/MeetTeam.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./meetTeam.scss";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+const MeetTeam = ({
+  img,
+  Aorder,
+  cap1,
+  cap2,
+  smtxt1,
+  smtxt2,
+  subtitle,
+  text,
+}) => {
+  return (
+    <div className="Mttcard">
+      <img src={img} style={{ order: Aorder }} alt="" />
+
+      <div className="MttInfo">
+        <h3>
+          {cap1}
+          <span className="small_text">{smtxt1} </span>
+          {cap2}
+          <span className="small_text">{smtxt2} </span>
+        </h3>
+        <p className="subTitle">{subtitle}</p>
+        <p className="text">{text}</p>
+        <div className="icon-grp">
+          <span className="icon">
+            <InstagramIcon />
+          </span>
+          <span className="icon">
+            <LinkedInIcon />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MeetTeam;
